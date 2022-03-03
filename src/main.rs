0b806bf22722e577af20653143c574c26f90ab9b
migmatore <kazakvova201@gmail.com>
2022-03-03 22:37:29 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/{name}")]
@@ -7,12 +9,17 @@ async fn greet_person(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be number");
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(|| async { "Hello World!" }))
             .service(greet_person)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
