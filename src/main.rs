a6effadc9491d05aff992fab332bf3977ef17aeb
migmatore <kazakvova201@gmail.com>
2022-03-05 02:13:01 +0300

    Enable logging middleware

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 
 use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::middleware::Logger;
+
+use env_logger::Env;
 
 #[get("/{name}")]
 async fn greet_person(name: web::Path<String>) -> impl Responder {
@@ -9,6 +12,8 @@ async fn greet_person(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
+
     let port = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
@@ -16,6 +21,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .route("/", web::get().to(|| async { "Hello World!" }))
             .service(greet_person)
     })
